8452e22ba56c6eacbdfb427434531b001726a621
TalhaTZA <talhavjtza@gmail.com>
2018-10-22 16:51:16 +0500

    module object practice

---
+++ helpers.js
@@ -0,0 +1,6 @@
+const object=require('./lib');
+
+
+console.log(object.title);
+
+console.log(object.fun(10,22));
---
+++ lib.js
@@ -0,0 +1,9 @@
+
+
+module.exports.title = 'Title Page';
+
+module.exports.fun=function (val1,val2) {
+    return val1+val2;
+}
+
+//console.log(module.exports);
