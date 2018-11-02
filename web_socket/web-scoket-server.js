22c65c73bf4a6182e6799ece2457a4c5cfaa61fe
TalhaTZA <talhavjtza@gmail.com>
2018-11-02 16:49:15 +0500

    made small chat app using web sockets

--- web_socket/web-scoket-server.js
+++ web_socket/web-scoket-server.js
@@ -4,4 +4,20 @@ const WSS=new webSocketServer({port:4000});
 
 WSS.on('connection',(ws)=>{
     console.log("Connected to server");
-})
+
+    ws.on('message',(message)=>{
+        console.log(message);
+
+        if(message==='close'){
+            ws.close();
+        }
+
+        else{
+            WSS.clients.forEach((client)=>{
+                client.send(message);
+            });
+        }
+        
+    });
+
+});
--- web_socket/web-socket-client.js
+++ web_socket/web-socket-client.js
@@ -1,1 +1,37 @@
 const WS=new WebSocket('ws://localhost:4000');
+
+WS.onmessage = (payload)=>{
+    //console.log(payload.data);
+    displayMessages(payload.data);
+};
+
+WS.onopen = ()=>{
+    displayTitle("CONNECTION OPEN");
+};
+
+WS.onclose = ()=>{
+    displayTitle("CONNECTION CLOSE");
+};
+
+
+function displayTitle(title){
+    document.querySelector("h1").innerHTML = title ;
+};
+
+function displayMessages(message){
+    const h1 = document.createElement("h1");
+    h1.innerHTML = message;
+    document.querySelector("div.messages").appendChild(h1);
+};
+
+document.forms[0].onsubmit = () =>{
+
+    let input = document.getElementById('message');
+
+    //console.log(input.value);
+
+    WS.send(input.value);
+
+    input.value="";
+
+};
