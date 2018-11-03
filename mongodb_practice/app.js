9ede22e57030b2183bad7cff908816ddb1ce82bd
TalhaTZA <talhavjtza@gmail.com>
2018-11-03 15:18:57 +0500

    ADD CRUD operations

--- mongodb_practice/app.js
+++ mongodb_practice/app.js
@@ -1,4 +1,4 @@
-const MongoClient = require('mongodb').MongoClient;
+const {MongoClient , ObjectId} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017',{ useNewUrlParser: true },(err,client)=>{
     if (err) { throw err};
@@ -7,6 +7,8 @@ MongoClient.connect('mongodb://localhost:27017',{ useNewUrlParser: true },(err,c
 
     const db = client.db("animals");
 
+    //CREATE
+
     // db.collection('mamals').insertOne({
     //     name: "Cat"
     // },(err,result)=>{
@@ -14,6 +16,41 @@ MongoClient.connect('mongodb://localhost:27017',{ useNewUrlParser: true },(err,c
 
     //     console.log(`inserted ${result}`);
     // });
+
+    //READ
+
+    db.collection('mamals').find().toArray((err,result)=>{
+        if (err) {throw err};
+
+        console.log(result);
+    });
+
+    //UPDATE
+
+    // db.collection("mamals").findOneAndUpdate(
+    //     {
+    //         _id: new ObjectId('5bdd6c7c4471832594d3a9a2')
+    //     } ,
+    //     {
+    //         $set : {
+    //             name: "horse",
+    //             legs: 4
+    //         }
+    //     }
     
+    // ).then(result =>{
+    //     console.log(result);
+    // }).catch(err => {throw err});
+
+
+    //DELETE
+
+    // db.collection("mamals").findOneAndDelete({
+
+    //     _id: new ObjectId('5bdd6c7c4471832594d3a9a2')
+    
+    // }).then(result=>{
+    //     console.log(result);
+    // }).catch(err=>{throw err});
 
 });
