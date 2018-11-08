e40a6f8fb94de0de7967c745b318f5a4c5c9c247
TalhaTZA <talhavjtza@gmail.com>
2018-11-08 21:10:02 +0500

    ADD express server

--- mongodb_practice/app_mongoose.js
+++ mongodb_practice/app_mongoose.js
@@ -1,7 +1,17 @@
 const mongoose= require('mongoose');
+const express= require('express');
 
+const app=express();
 
-mongoose.connect("mongodb://localhost:27017/practicemongoose",{useNewUrlParser : true});
-mongoose.connection
-    .once('open',()=>{console.log("Connected")})
-    .on('error',(err)=>{console.log(`Could not connect ${err}`)});
+const port = 3000 || process.env.PORT;
+
+app.listen(port,(err)=>{
+    if(err) {throw err;}
+
+    console.log(`Listening on port ${port}`);
+
+    mongoose.connect("mongodb://localhost:27017/practicemongoose",{useNewUrlParser : true});
+    mongoose.connection
+        .once('open',()=>{console.log("Connected To Database")})
+        .on('error',(err)=>{console.log(`Could not connect ${err}`)});
+});
