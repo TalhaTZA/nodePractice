b06d25f39d9aca2b4bb22b78f1afed0d269ddf29
TalhaTZA <talhavjtza@gmail.com>
2018-11-09 21:13:32 +0500

    ADD body parser package and ADD get request to get all users

--- mongodb_practice/app_mongoose.js
+++ mongodb_practice/app_mongoose.js
@@ -1,8 +1,11 @@
 const mongoose= require('mongoose');
 const express= require('express');
+const bodyParser=require('body-parser');
 const User=require('./models/User');
 
 const app=express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 const port = 3000 || process.env.PORT;
 
@@ -15,15 +18,28 @@ app.get('/',(req,res)=>{
 app.post('/save', async (req,res)=>{
 
     const user=new User({
-        firstName: "Talha",
-        lastName: "Ahmed"
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        isActive: req.body.isActive
     });
 
     try {
         const newUser = await  user.save();
         res.json(newUser);
     } catch (error) {
-        throw error;
+        res.status(400).send('User not Saved');
+    } 
+        //console.log(newUser);
+    
+});
+
+app.get('/users', async (req,res)=>{
+
+    try {
+        const users = await User.find({});
+        res.json(users);
+    } catch (error) {
+        res.status(400).send('Bad Request');
     } 
         //console.log(newUser);
     
